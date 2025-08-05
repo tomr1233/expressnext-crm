1181af138a53ec95d96c20a368f43ec8ec980d37
tomr1233 <tomr@expresstxtapp.com>
2025-08-05 20:41:43 +1200

    fix login page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -34,7 +34,7 @@ export default function AuthPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
           <h2 className="mt-6 text-3xl font-extrabold text-gray">
